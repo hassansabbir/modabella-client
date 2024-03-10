8ee45a07dabcd69e3c12133f69ce4ea4b79b3ae2
hassansabbir <mahmoodsabbir3087@gmail.com>
2024-03-10 22:18:58 +0600

    elu

--- src/Pages/Dashboard/ManageSellerReq/ManageSellerReq.jsx
+++ src/Pages/Dashboard/ManageSellerReq/ManageSellerReq.jsx
@@ -2,7 +2,7 @@ const ManageSellerReq = () => {
   return (
     <div>
       <h2 className="text-5xl font-display text-center my-20 font-semibold">
-        Manage Seller Requests From Users
+        All the seller requests
       </h2>
     </div>
   );
