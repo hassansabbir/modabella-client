3765d49ea0e05be3d52d2d84ad5af55fa6174517
hassansabbir <mahmoodsabbir3087@gmail.com>
2024-11-14 13:52:25 +0600

    updated

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,29 +1,33 @@
 const About = () => {
   return (
-    <div className="pt-24 max-w-7xl mx-auto">
-      <h2 className="text-5xl font-bold text-center">Welcome to ModaBella</h2>
-      <p className="text-xl text-justify my-5">
-        At Modabella, we believe that fashion is not just about clothing; it's a
-        form of self-expression and empowerment. Our journey began with a
-        passion for helping people of all ages and genders discover their unique
-        style, and it has since grown into a thriving e-commerce platform.
-        Located in the heart of the fashion industry, we curate a diverse
-        collection of men's, women's, and children's clothing that embraces
-        individuality and inclusivity.
-      </p>
-      <h2 className="text-5xl font-bold text-center">Our Commitment to You</h2>
-      <p className="text-xl my-5 text-justify">
-        Modabella is more than just a fashion destination; it's a reflection of
-        our commitment to quality, style, and customer satisfaction. We stand
-        behind the principles of authenticity, sustainability, and exceptional
-        service. Our handpicked selections are a testament to our dedication to
-        offering you the latest trends, timeless classics, and eco-conscious
-        options. With Modabella, you're not just shopping for clothing; you're
-        embarking on a style journey that celebrates your unique personality and
-        story. Thank you for choosing us as your fashion partner, and we can't
-        wait to help you discover your perfect look. Welcome to the Modabella
-        family!
-      </p>
+    <div className="md:flex justify-evenly">
+      <div className="pt-24 max-w-7xl mx-auto">
+        <h2 className="text-5xl font-bold text-center">Welcome to ModaBella</h2>
+        <p className="text-xl text-justify my-5">
+          At Modabella, we believe that fashion is not just about clothing; it's
+          a form of self-expression and empowerment. Our journey began with a
+          passion for helping people of all ages and genders discover their
+          unique style, and it has since grown into a thriving e-commerce
+          platform. Located in the heart of the fashion industry, we curate a
+          diverse collection of men's, women's, and children's clothing that
+          embraces individuality and inclusivity.
+        </p>
+        <h2 className="text-5xl font-bold text-center">
+          Our Commitment to You
+        </h2>
+        <p className="text-xl my-5 text-justify">
+          Modabella is more than just a fashion destination; it's a reflection
+          of our commitment to quality, style, and customer satisfaction. We
+          stand behind the principles of authenticity, sustainability, and
+          exceptional service. Our handpicked selections are a testament to our
+          dedication to offering you the latest trends, timeless classics, and
+          eco-conscious options. With Modabella, you're not just shopping for
+          clothing; you're embarking on a style journey that celebrates your
+          unique personality and story. Thank you for choosing us as your
+          fashion partner, and we can't wait to help you discover your perfect
+          look. Welcome to the Modabella family!
+        </p>
+      </div>
     </div>
   );
 };
--- src/routes/PrivetRoute.jsx
+++ src/routes/PrivetRoute.jsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import { Navigate, useLocation } from "react-router-dom";
 import { AuthContext } from "../Providers/AuthProvider";
-import { Vortex } from "react-loader-spinner";
+// import { Vortex } from "react-loader-spinner";
 
 const PrivetRoute = ({ children }) => {
   const { user, loading } = useContext(AuthContext);
@@ -9,15 +9,7 @@ const PrivetRoute = ({ children }) => {
   if (loading) {
     return (
       <div className="flex justify-center mt-72">
-        <Vortex
-          visible={true}
-          height="80"
-          width="80"
-          ariaLabel="vortex-loading"
-          wrapperStyle={{}}
-          wrapperClass="vortex-wrapper"
-          colors={["red", "green", "blue", "yellow", "orange", "purple"]}
-        />
+        <span className="loading loading-dots loading-lg"></span>
       </div>
     );
   }
